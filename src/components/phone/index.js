c362e90e0c7b95ab884d6d4b097f79ed77d2437f
Sreepriya Sreekumar <sreepriyaks@gmail.com>
2018-08-20 22:02:34 +0530

    edit/delete/back buttons on single phone page

--- src/components/phone/index.js
+++ src/components/phone/index.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as R from 'ramda';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 
 import { fetchPhoneById } from '../../actions';
 
 class Phone extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalIsOpen: false };
+  }
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleModalCloseRequest = () => {
+    // opportunity to validate something and keep the modal open even if it
+    // requested to be closed
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleSaveClicked = e => {
+    alert('Save button was clicked');
+  };
+
   componentDidMount() {
     this.props.fetchPhoneById(this.props.match.params.id);
   }
@@ -37,6 +61,7 @@ class Phone extends Component {
 
   renderContent() {
     const { phone } = this.props;
+
     return (
       <div className="mg-t text-dark">
         <div className="row">
@@ -58,7 +83,7 @@ class Phone extends Component {
 
   render() {
     const { phone } = this.props;
-
+    console.log(phone.name);
     return (
       <div className="view-container">
         <nav
@@ -84,12 +109,183 @@ class Phone extends Component {
             </div>
           </div>
         </nav>
+
+        <div>
+          <Modal
+            className="Modal__Bootstrap modal-dialog text-dark mg-t"
+            closeTimeoutMS={150}
+            isOpen={this.state.modalIsOpen}
+            onRequestClose={this.handleModalCloseRequest}
+          >
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Edit Phone</h4>
+                <button
+                  type="button"
+                  className="close"
+                  onClick={this.handleModalCloseRequest}
+                >
+                  <span aria-hidden="true">&times;</span>
+                  <span className="sr-only">Close</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="Phone Name"
+                    defaultValue={phone.name}
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control"
+                    id="description"
+                    name="description"
+                    placeholder="Description"
+                    defaultValue={phone.description}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="price"
+                    name="price"
+                    placeholder="Price"
+                    defaultValue={phone.price}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="cpu"
+                    name="cpu"
+                    placeholder="CPU"
+                    defaultValue={phone.cpu}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="camera"
+                    name="camera"
+                    placeholder="Camera"
+                    defaultValue={phone.camera}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="size"
+                    name="size"
+                    placeholder="Size"
+                    defaultValue={phone.size}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="weight"
+                    name="weight"
+                    placeholder="Weight"
+                    defaultValue={phone.weight}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="display"
+                    name="display"
+                    placeholder="Display"
+                    defaultValue={phone.display}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="battery"
+                    name="battery"
+                    placeholder="Battery"
+                    defaultValue={phone.battery}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="memory"
+                    name="memory"
+                    placeholder="Memory"
+                    defaultValue={phone.memory}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="file"
+                    className="form-control-file"
+                    id="image"
+                    name="image"
+                    placeholder="Upload Image"
+                  />
+                </div>
+                <div className="checkbox" />
+              </div>
+              <div className="modal-footer">
+                <button className="btn btn-success" data-dismiss="modal">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </Modal>
+        </div>
+
         <div className="container">
           <div className="row">
             <div className="col-md-12 col-sm-12 col-lg-12 App-margin-10">
               {phone && this.renderContent()}
+              <div className="float-right">
+                <button
+                  className="btn btn-outline-success mr-3"
+                  onClick={this.openModal}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn btn-outline-danger mr-3"
+                  data-toggle="#confirmDelete"
+                >
+                  Delete
+                </button>
+                <button className="btn btn-outline-info mr-3">Back</button>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div
+          class="container alert alert-danger alert-dismissible fixed-top"
+          id="confirmDelete"
+        >
+          <button class="close" type="button" data-dismiss="alert">
+            <span>&times;</span>
+          </button>
+          <h4 class="alert-heading">Are you sure?</h4>
+          <p>Continue Deleting the Phone?</p>
+          <hr />
+          <button class="btn btn-danger">Yes</button>
+          <button class="btn btn-info" data-dismiss="alert">
+            No
+          </button>
         </div>
       </div>
     );
